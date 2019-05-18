634016a031970634201f63261e51aa2147303a4e
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-05-18 06:40:38 -0500

    fixed formatting , IDE options for VB.Net and commenting

---
+++ TemperatureConversion/TemperatureConversion/TemperatureConversion.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TemperatureConversion
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+    Me.cboFrom = New System.Windows.Forms.ComboBox()
+    Me.cboTo = New System.Windows.Forms.ComboBox()
+    Me.btnConvert = New System.Windows.Forms.Button()
+    Me.btnReset = New System.Windows.Forms.Button()
+    Me.btnExit = New System.Windows.Forms.Button()
+    Me.lblFrom = New System.Windows.Forms.Label()
+    Me.lblTo = New System.Windows.Forms.Label()
+    Me.txtFrom = New System.Windows.Forms.TextBox()
+    Me.lblOutput = New System.Windows.Forms.Label()
+    Me.SuspendLayout()
+    '
+    'cboFrom
+    '
+    Me.cboFrom.FormattingEnabled = True
+    Me.cboFrom.Items.AddRange(New Object() {"FAHRENHEIT", "CELSIUS", "KELVIN"})
+    Me.cboFrom.Location = New System.Drawing.Point(164, 24)
+    Me.cboFrom.Name = "cboFrom"
+    Me.cboFrom.Size = New System.Drawing.Size(121, 21)
+    Me.cboFrom.TabIndex = 0
+    '
+    'cboTo
+    '
+    Me.cboTo.FormattingEnabled = True
+    Me.cboTo.Items.AddRange(New Object() {"FAHRENHEIT", "CELSIUS", "KELVIN"})
+    Me.cboTo.Location = New System.Drawing.Point(164, 49)
+    Me.cboTo.Name = "cboTo"
+    Me.cboTo.Size = New System.Drawing.Size(121, 21)
+    Me.cboTo.TabIndex = 1
+    '
+    'btnConvert
+    '
+    Me.btnConvert.Location = New System.Drawing.Point(16, 87)
+    Me.btnConvert.Name = "btnConvert"
+    Me.btnConvert.Size = New System.Drawing.Size(75, 23)
+    Me.btnConvert.TabIndex = 2
+    Me.btnConvert.Text = "Convert"
+    Me.btnConvert.UseVisualStyleBackColor = True
+    '
+    'btnReset
+    '
+    Me.btnReset.Location = New System.Drawing.Point(114, 87)
+    Me.btnReset.Name = "btnReset"
+    Me.btnReset.Size = New System.Drawing.Size(75, 23)
+    Me.btnReset.TabIndex = 3
+    Me.btnReset.Text = "Reset"
+    Me.btnReset.UseVisualStyleBackColor = True
+    '
+    'btnExit
+    '
+    Me.btnExit.Location = New System.Drawing.Point(210, 86)
+    Me.btnExit.Name = "btnExit"
+    Me.btnExit.Size = New System.Drawing.Size(75, 23)
+    Me.btnExit.TabIndex = 4
+    Me.btnExit.Text = "Exit"
+    Me.btnExit.UseVisualStyleBackColor = True
+    '
+    'lblFrom
+    '
+    Me.lblFrom.AutoSize = True
+    Me.lblFrom.Location = New System.Drawing.Point(12, 27)
+    Me.lblFrom.Name = "lblFrom"
+    Me.lblFrom.Size = New System.Drawing.Size(33, 13)
+    Me.lblFrom.TabIndex = 5
+    Me.lblFrom.Text = "From:"
+    '
+    'lblTo
+    '
+    Me.lblTo.AutoSize = True
+    Me.lblTo.Location = New System.Drawing.Point(12, 53)
+    Me.lblTo.Name = "lblTo"
+    Me.lblTo.Size = New System.Drawing.Size(23, 13)
+    Me.lblTo.TabIndex = 6
+    Me.lblTo.Text = "To:"
+    '
+    'txtFrom
+    '
+    Me.txtFrom.Location = New System.Drawing.Point(59, 24)
+    Me.txtFrom.Name = "txtFrom"
+    Me.txtFrom.Size = New System.Drawing.Size(100, 20)
+    Me.txtFrom.TabIndex = 7
+    '
+    'lblOutput
+    '
+    Me.lblOutput.AutoSize = True
+    Me.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+    Me.lblOutput.Location = New System.Drawing.Point(59, 49)
+    Me.lblOutput.MaximumSize = New System.Drawing.Size(100, 15)
+    Me.lblOutput.MinimumSize = New System.Drawing.Size(100, 20)
+    Me.lblOutput.Name = "lblOutput"
+    Me.lblOutput.Size = New System.Drawing.Size(100, 20)
+    Me.lblOutput.TabIndex = 8
+    '
+    'TemperatureConversion
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(297, 122)
+    Me.Controls.Add(Me.lblOutput)
+    Me.Controls.Add(Me.txtFrom)
+    Me.Controls.Add(Me.lblTo)
+    Me.Controls.Add(Me.lblFrom)
+    Me.Controls.Add(Me.btnExit)
+    Me.Controls.Add(Me.btnReset)
+    Me.Controls.Add(Me.btnConvert)
+    Me.Controls.Add(Me.cboTo)
+    Me.Controls.Add(Me.cboFrom)
+    Me.Name = "TemperatureConversion"
+    Me.Text = "Temperature Conversion"
+    Me.ResumeLayout(False)
+    Me.PerformLayout()
+
+  End Sub
+
+  Friend WithEvents cboFrom As ComboBox
+  Friend WithEvents cboTo As ComboBox
+  Friend WithEvents btnConvert As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblFrom As Label
+    Friend WithEvents lblTo As Label
+    Friend WithEvents txtFrom As TextBox
+  Friend WithEvents lblOutput As Label
+End Class
---
+++ TemperatureConversion/TemperatureConversion/TemperatureConversion.vb
@@ -0,0 +1,215 @@
+﻿' Copyright (c) 2019 Applied Systems, Inc.
+'
+
+''' -----------------------------------------------------------------------------
+''' Project     : TemperatureConversion
+''' Class       : TemperatureConversion
+'''
+''' This project is for intern Alberto Pizano's code review.
+'''
+''' -----------------------------------------------------------------------------
+''' <summary>
+'''   First project from BusProd Developer Training/Temp Conversion on appliedwiki.
+''' </summary>
+''' <remarks>
+''' </remarks>
+''' <history>
+'''     [apizano]       5/16/2019       Reformated comments.
+'''     [apizano]       5/15/2019       Checked-in code to TFS source control.
+''' </history>
+''' -----------------------------------------------------------------------------
+
+Public Class TemperatureConversion
+
+  Enum Temp
+    F
+    C
+    K
+  End Enum
+
+
+  Private Sub txtFrom_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtFrom.KeyPress
+    'If statement Makes sure only numeric 0-9, backspace, dash for negative sign (-), and decimal point (.) characters allowed in TextBox.
+    If Not Char.IsDigit(e.KeyChar) And Not Char.IsControl(e.KeyChar) And Not e.KeyChar = "." And Not e.KeyChar = "-" Then
+      e.Handled = True
+    End If
+
+    'If statement makes sure total of one decimal point (.) is allowed in TextBox.
+    If txtFrom.Text.IndexOf(".") >= 0 And e.KeyChar = "." Then
+      e.Handled = True
+    End If
+
+    'If statement to make sure dash (-) only allowed at index 0 of TextBox.
+    If Not String.IsNullOrEmpty(txtFrom.Text) And e.KeyChar = "-" Then
+      e.Handled = True
+    End If
+  End Sub
+
+
+  Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+    txtFrom.Text = String.Empty
+    lblOutput.Text = String.Empty
+    cboFrom.SelectedIndex = -1
+    cboTo.SelectedIndex = -1
+  End Sub
+
+  Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+    If MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButtons.OKCancel) = DialogResult.OK Then
+      Application.Exit()
+    End If
+  End Sub
+
+  Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+    'Initializes temperature input to "From:" TextBox to Double variable type.
+    Dim dValueOfTxtBoxFrom As Double = 0
+
+    'Error Handling for Drop-down boxes.
+    'Get values from both Drop-down boxes and store inside variables if not NULL. Else throws NULL object reference Error 10001.
+    If Me.cboFrom.SelectedIndex = -1 AndAlso Me.cboTo.SelectedIndex = -1 Then
+      MessageBox.Show("Please select temperature units to convert from and to.", "Error: 10001", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+      'txtFrom.Select()
+      Me.cboFrom.Focus()
+      'lblOutput.Select()
+    ElseIf cboFrom.SelectedIndex = -1 OrElse cboTo.SelectedIndex = -1 Then
+      MessageBox.Show("Please select temperature units to convert from and to.", "Error: 10001", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+      'txtFrom.Select()
+      'cboTo.Focused()
+      'End If
+      'lblOutput.Select()
+    End If
+
+
+    'Main logic temperature conversion program. Selects which operation to perform depending on which sDrpDownSelect1 value (FAHRENHEIT, CELSIUS, KELVIN) is selected.
+    'Performs appropriate mathematical conversion depending on which sDrpDownSelect2 value (FAHRENHEIT, CELSIUS, KELVIN) is selected.
+    'If cboFrom.SelectedIndex <> -1 And cboTo.SelectedIndex <> -1 And cboFrom.SelectedIndex <> cboTo.SelectedIndex Then
+    If cboFrom.SelectedIndex = cboTo.SelectedIndex AndAlso cboFrom.SelectedIndex <> -1 AndAlso cboTo.SelectedIndex <> -1 Then
+      lblOutput.Text = txtFrom.Text
+
+      'ElseIf cboFrom.SelectedIndex = cboTo.SelectedIndex And cboFrom.SelectedIndex <> -1 And cboTo.SelectedIndex <> -1 Then
+    ElseIf cboFrom.SelectedIndex <> -1 AndAlso cboTo.SelectedIndex <> -1 AndAlso cboFrom.SelectedIndex <> cboTo.SelectedIndex Then
+      Select Case cboFrom.SelectedIndex
+        Case Temp.F
+          If cboTo.SelectedIndex = Temp.C Then
+            dValueOfTxtBoxFrom = CType(txtFrom.Text, Double)
+            Try
+              lblOutput.Text = CType(FahrToCel(dValueOfTxtBoxFrom), String)
+            Catch ex1 As ArithmeticException
+              Console.WriteLine(ex1.Message)
+            End Try
+          Else
+            dValueOfTxtBoxFrom = CType(txtFrom.Text, Double)
+            Try
+              lblOutput.Text = CType(FahrToKelv(dValueOfTxtBoxFrom), String)
+            Catch ex2 As ArithmeticException
+              Console.WriteLine(ex2.Message)
+            End Try
+          End If
+        Case Temp.C
+          If cboTo.SelectedIndex = Temp.F Then
+            dValueOfTxtBoxFrom = CType(txtFrom.Text, Double)
+            Try
+              lblOutput.Text = CType(CelToFahr(dValueOfTxtBoxFrom), String)
+            Catch ex3 As ArithmeticException
+              Console.WriteLine(ex3.Message)
+            End Try
+          Else
+            dValueOfTxtBoxFrom = CType(txtFrom.Text, Double)
+            Try
+              lblOutput.Text = CType(CelToKelv(dValueOfTxtBoxFrom), String)
+            Catch ex4 As ArithmeticException
+              Console.WriteLine(ex4.Message)
+            End Try
+          End If
+        Case Temp.K
+          If cboTo.SelectedIndex = Temp.F Then
+            dValueOfTxtBoxFrom = CType(txtFrom.Text, Double)
+            Try
+              lblOutput.Text = CType(KelvToFahr(dValueOfTxtBoxFrom), String)
+            Catch ex5 As ArithmeticException
+              Console.WriteLine(ex5.Message)
+            End Try
+          Else
+            dValueOfTxtBoxFrom = CType(txtFrom.Text, Double)
+            Try
+              lblOutput.Text = CType(KelvToCel(dValueOfTxtBoxFrom), String)
+            Catch ex6 As ArithmeticException
+              Console.WriteLine(ex6.Message)
+            End Try
+          End If
+      End Select
+    End If
+  End Sub
+
+  ''' <summary>
+  '''   Mathematical conversion formula to convert FAHRENHEIT to CELSIUS that is called within main logic above Select...Case statements.
+  ''' </summary>
+  ''' <param name="dNum">The double temperature value from the "From:" TextBox that will be used for conversion.</param>
+  ''' <remarks>
+  ''' </remarks>
+  Dim dAns As Double = 0
+  Function FahrToCel(dNum As Double) As Double
+    dAns = (dNum - 32) * 5 / 9
+    dAns = Math.Round(dAns, 2)
+    Return dAns
+  End Function
+
+  ''' <summary>
+  '''   Mathematical conversion formula to convert FAHRENHEIT to KELVIN that is called within main logic above Select...Case statements.
+  ''' </summary>
+  ''' <param name="dNum">The double temperature value from the "From:" TextBox that will be used for conversion.</param>
+  ''' <remarks>
+  ''' </remarks>
+  Function FahrToKelv(dNum As Double) As Double
+    dAns = (dNum - 32) * 5 / 9 + 273.15
+    dAns = Math.Round(dAns, 2)
+    Return dAns
+  End Function
+
+  ''' <summary>
+  '''   Mathematical conversion formula to convert CELSIUS to FAHRENHEIT that is called within main logic above Select...Case statements.
+  ''' </summary>
+  ''' <param name="dNum">The double temperature value from the "From:" TextBox that will be used for conversion.</param>
+  ''' <remarks>
+  ''' </remarks>
+  Function CelToFahr(dNum As Double) As Double
+    dAns = (dNum * 9 / 5) + 32
+    dAns = Math.Round(dAns, 2)
+    Return dAns
+  End Function
+
+  ''' <summary>
+  '''   Mathematical conversion formula to convert CELSIUS to KELVIN that is called within main logic above Select...Case statements.
+  ''' </summary>
+  ''' <param name="dNum">The double temperature value from the "From:" TextBox that will be used for conversion.</param>
+  ''' <remarks>
+  ''' </remarks>
+  Function CelToKelv(dNum As Double) As Double
+    dAns = dNum + 273.15
+    dAns = Math.Round(dAns, 2)
+    Return dAns
+  End Function
+
+  ''' <summary>
+  '''   Mathematical conversion formula to convert KELVIN to FAHRENHEIT that is called within main logic above Select...Case statements.
+  ''' </summary>
+  ''' <param name="dNum">The double temperature value from the "From:" TextBox that will be used for conversion.</param>
+  ''' <remarks>
+  ''' </remarks>
+  Function KelvToFahr(dNum As Double) As Double
+    dAns = (dNum - 273.15) * 9 / 5 + 32
+    dAns = Math.Round(dAns, 2)
+    Return dAns
+  End Function
+
+  ''' <summary>
+  '''   Mathematical conversion formula to convert KELVIN to CELSIUS that is called within main logic above Select...Case statements.
+  ''' </summary>
+  ''' <param name="dNum">The double temperature value from the "From:" TextBox that will be used for conversion.</param>
+  ''' <remarks>
+  ''' </remarks>
+  Function KelvToCel(dNum As Double) As Double
+    dAns = dNum - 273.15
+    dAns = Math.Round(dAns, 2)
+    Return dAns
+  End Function
+End Class
